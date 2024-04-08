8bc1c25b089eed306d740b04f09611d1f335888e
gt3431 <gt3431@mci4me.at>
2024-04-08 13:47:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from load_data import load_data
+from sort import bubble_sort
+
+if __name__ == "__main__":
+    data = load_data('data/activity.csv')
+    power_W = data['PowerOriginal']
+    print(power_W)
+    sorted_power_W = bubble_sort(power_W)
+    print(sorted_power_W[::-1])
