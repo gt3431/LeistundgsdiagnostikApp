f9e1b1ecb66db97ded3d5523afd7f3407cf12bf7
[Lucas <[fl1940@mci4me.at]>
2024-04-08 14:49:41 +0200

    first plot

--- source/main.py
+++ source/main.py
@@ -1,9 +1,21 @@
 from load_data import load_data
 from sort import bubble_sort
+import matplotlib.pyplot as plt
+
 
 if __name__ == "__main__":
     data = load_data('data/activity.csv')
     power_W = data['PowerOriginal']
     print(power_W)
     sorted_power_W = bubble_sort(power_W)
-    print(sorted_power_W[::-1])
+    print(sorted_power_W[::-1])     
+
+plt.plot(sorted_power_W[::-1])
+
+# Achsenbeschriftungen hinzufügen
+# Titel hinzufügen
+# plt.title('Einfaches Liniendiagramm')
+
+# Diagramm anzeigen
+plt.savefig('figures/sortetpower.png')
+plt.show()
